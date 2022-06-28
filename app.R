8289a519e0c1dbebfb5ae00c13e7842240c28273
JaY 3 <jeanalainpro2@gmail.com>
2022-06-28 17:00:53 +0200

    root starting UI

--- app.R
+++ app.R
@@ -1,21 +1,23 @@
 # Start with update
 # Be sure RAM is empty at start
 rm(list=ls())
-
+library(tidyverse)
+library(shiny)
 #source("update.R")
 # Create  missing_imports list + Import the missing_files 
 
-require(shiny)
 
-#-----------------------------------
-# Import cmd files
-#require(readxl)
-#PATH = "./inputs/2017/cmd_files/fichier_commande_general.xlsx"
-#excel_sheets(PATH)
-#read_excel(PATH,sheet = "Typologie_A")
-#---------------------------------------------
 
-server <-function(input,output){
+server <- function(input,output){
+    TC1 <-read.csv("./outputs/2017/TC1.csv")
+    data_for_plot<-TC1 %>% filter(typo_A_det=="Ensemble" & typo_A=="Ensemble des secteurs d'activité" & famille=="Ensemble" & ESS=="ESS + Hors ESS")%>% select(DEP, rem_brut)
+    data_for_plot$rem_brut<-as.numeric(data_for_plot$rem_brut)
+    
+    
+    output$plot <- renderPlot({
+        barplot(rem_brut ~ DEP,data=data_for_plot)    
+    })
+
 
 }
 
